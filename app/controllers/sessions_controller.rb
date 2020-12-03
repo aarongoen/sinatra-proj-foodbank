e717f09be3d2b3f3d314936282d4a26b79914ac9
aarongoen <aarongoen2.0@gmail.com>
2020-12-03 10:01:44 -0500

    Removes unused commentsed out success and failure routes in Session Controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,18 +10,14 @@ class SessionsController < ApplicationController
     
     post '/sessions/login' do
       @user = User.find_by(username: params[:username])
-      # binding.pry
           if params["username"].empty? || params["password"].empty?
             @error = "You must enter a username and password. Please try again."
             erb :'/sessions/login'
           elsif 
-            # binding.pry
             @user && @user.authenticate(params[:password]) 
             session[:user_id] = @user.id
-            # binding.pry
             erb :'/food_requests/index'
           elsif 
-            # binding.pry
             !@user.authenticate(params[:password])
             @error = "Incorrect password"
             erb :'/sessions/login'
@@ -31,23 +27,6 @@ class SessionsController < ApplicationController
           end
 
     end
-    
-      # get "/success" do
-        # 	if logged_in?
-        # 		erb :success
-        # 	else
-        # 		redirect to :login
-        # 	end
-      # end
-    
-      # get "/failure" do
-        # 	erb :failure
-        # end
-      
-      # # GET: /users
-      # get "/users" do
-      #   erb :"/users/index.html"
-      # end
     
       get '/sessions/logout' do
         session.clear
